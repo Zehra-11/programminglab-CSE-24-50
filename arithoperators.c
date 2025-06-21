59a4f283c5ca27034a13e85ea13e8a45d528f1d4
Zehra-11 <syedafaru75@gmail.com>
2025-06-21 12:04:15 +0530

    Add files via upload

---
+++ arithoperators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+// + addition
+// - subtraction
+// * multiplication
+// / division
+// % modulur operator or modulo  (gives remainder of any division)
+// ++ increment
+// -- decrement
+int  x = 5;
+int  y = 2;
+//int z = x + y;
+//int z = x - y;
+//int z = x * y;
+//float z = x/(float)y; to get 2.5, divisor ko  ya float banao ya upr hi float banao,%f used for z
+//int z = x % y;
+x++;
+y--;
+
+
+printf("%d",y);
+return 0;
+}
